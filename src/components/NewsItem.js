5bbcae58dd1ef4255373026762e4a4e0daba0832
Victoria Khatian <khatian.v@gmail.com>
2022-06-09 18:58:48 +0100

    search added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,7 +3,7 @@ import React from "react";
 //as per our design we are passing title, description and image as props
 function NewsItem({ title, description, image, url }) {
   return (
-    <div>
+    <div className="container">
       <div className="news-item">
         <a href={url}>
           <img className="news-image" src={image} alt="" />
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -5,22 +5,39 @@ import NewsItem from "./NewsItem";
 
 function NewsList() {
   const [list, setList] = useState([]); //list of news per page
+  const [text, setText] = useState(""); //
+  const [search, setSearch] = useState("UK");
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
-        "https://gnews.io/api/v4/search?q=example&token=2699378b76ff6c77e3ebe7387a1e95da"
+        `https://gnews.io/api/v4/search?q=${search}&lang=en&token=7bafd4fddd55d7bf781b2a8bce7a696e`
       );
-      const data = await response.json(); //we want our data in json format
+      const data = await response.json(); //getting data by search( in json format )
       console.log(data);
       setList(data.articles);
     };
     fetchData();
-  }, []); //we want our data to be loaded just once, so empty array
+  }, [search]); //we want our data to be loaded just once, so empty array
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  function handleClick() {
+    setSearch(text);
+    console.log(text);
+  }
 
   //we are rendering all the articles that we've fetched (max 10)
   return (
     <div>
+      <div className="search">
+        <input onChange={handleChange} placeholder="Search" />
+        <button onClick={handleClick} className="button">
+          Submit
+        </button>
+      </div>
       {list.map((item) => {
         return (
           <NewsItem
